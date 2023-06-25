c1293b664bbff38f1b3339a98470bb260cd941bf
Hendry Lie <hendrylie188@gmail.com>
2023-06-26 01:17:54 +0700

    add faq page

--- lib/pages/main_page/profile/faq.dart
+++ lib/pages/main_page/profile/faq.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:testing/pages/main_page/profile/profile.dart';
 
 class Help extends StatefulWidget {
-  const Help({super.key});
+  const Help({Key? key});
 
   @override
   State<Help> createState() => _HelpState();
@@ -13,19 +11,138 @@ class _HelpState extends State<Help> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text(
-        'Help',
-        style: TextStyle(color: Colors.white),
+      appBar: AppBar(
+        title: const Text(
+          'Help',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+          color: Colors.white,
+        ),
       ),
-      centerTitle: true,
-      leading: IconButton(
-        onPressed: () {
-          Navigator.pop(context);
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => FAQ()),
+            );
+          },
+          child: Text('Open FAQ'),
+        ),
+      ),
+    );
+  }
+}
+
+class FAQ extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'FAQ',
+          style: TextStyle(color: Colors.white),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Frequently Asked Questions',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(height: 16.0),
+              FAQItem(
+                question: 'Question 1?',
+                answer: 'Answer 1',
+              ),
+              SizedBox(height: 16.0),
+              FAQItem(
+                question: 'Question 2?',
+                answer: 'Answer 2',
+              ),
+              // Add more FAQItems as needed
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FAQItem extends StatefulWidget {
+  final String question;
+  final String answer;
+
+  const FAQItem({
+    required this.question,
+    required this.answer,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  _FAQItemState createState() => _FAQItemState();
+}
+
+class _FAQItemState extends State<FAQItem> {
+  bool isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: ExpansionPanelList(
+        elevation: 0,
+        expandedHeaderPadding: EdgeInsets.zero,
+        expansionCallback: (int index, bool isExpanded) {
+          setState(() {
+            this.isExpanded = !isExpanded;
+          });
         },
-        icon: const Icon(Icons.arrow_back_ios),
-        color: Colors.white,
+        children: [
+          ExpansionPanel(
+            headerBuilder: (BuildContext context, bool isExpanded) {
+              return ListTile(
+                title: Text(
+                  widget.question,
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                ),
+              );
+            },
+            body: Padding(
+              padding: EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
+              child: Text(
+                widget.answer,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[600],
+                ),
+              ),
+            ),
+            isExpanded: isExpanded,
+          ),
+        ],
       ),
-    ));
+    );
   }
 }
